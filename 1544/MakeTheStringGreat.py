75230651da66aca77049a731aca3f0ee0f219610
Giovanni Esteban Moreno Urbina <ingestebanmu@hotmail.com>
2024-04-05 22:08:20 -0500

    añadir 1544

---
+++ 1544/MakeTheStringGreat.py
@@ -0,0 +1,8 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        nuevaPalabra = []
+        for letra in s:
+            if nuevaPalabra and abs(ord(nuevaPalabra[-1]) - ord(letra)) == 32:
+                nuevaPalabra.pop()
+            else:
+                nuevaPalabra.append(letra)
